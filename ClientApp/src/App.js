84a297e1c89d5654e9ecfa713952c011d33ec4cd
Sarathchandra Nvg <sarath.chandra.n.v.g@spglobal.com>
2021-07-14 13:47:29 +0530

    Add project files.

---
+++ ClientApp/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+import { FetchData } from './components/FetchData';
+import { Counter } from './components/Counter';
+import Header from './components/Layout/Header';
+import './custom.css';
+
+export default class App extends Component {
+    static displayName = App.name;
+
+    render() {
+        return (
+            <>
+                <Header />
+            </>
+        );
+    }
+}
---
+++ ClientApp/src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+﻿import React from 'react'
+import mealsImg from '../../assets/meals.jpg'
+import css from './Header.module.css'
+
+const Header = props => {
+    return(
+    <>
+        <div className={css.header}>
+            <h1>VEGAN HEALTY BITES</h1>
+            <button>Cart</button>
+        </div>
+        <div className={css['main-image']}>
+            <img src={mealsImg} alt="Food Items  Image" />
+        </div>
+    </>)
+}
+export default Header
